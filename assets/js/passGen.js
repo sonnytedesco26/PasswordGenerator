098f6037eef15ad45cf109bc27499a3ae6d19030
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-01-14 16:37:35 -0500

    initial push; added most js functionality

---
+++ assets/js/passGen.js
@@ -0,0 +1,60 @@
+
+/*var password=document.getElementById("pssLength");
+
+function generate() {
+    var passwordLength = document.getElementById("pssLength").value;
+    console.log("Value is " + passwordLength);
+}*/
+
+var password=document.getElementById("password");
+
+function generate() {
+    var password = "";
+    var lowercaseSet = document.getElementById("lowercase").checked;
+    var uppercaseSet = document.getElementById("uppercase").checked;
+    var numericSet = document.getElementById("numeric").checked;
+    var specialSet = document.getElementById("special").checked;
+    var passwordLength = document.getElementById("pssLength").value;  
+    var chars = "";
+    if(lowercaseSet){
+        chars += "abcdefghijklmnopqrstuvwxyz";
+    }
+    if(uppercaseSet){
+        chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if(numericSet){
+        chars += "0123456789"
+    }
+    if(specialSet){
+        chars += "!@#$%^&*()"
+    }
+    if(chars.length === 0){
+        //ERROR HANDLING
+    }
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber +1);
+    }
+    document.getElementById("password").value = password;
+}
+
+
+/*
+function generate() {
+    var password = "";
+    //var passwordLength = 8;
+    var passwordLength = document.getElementById("pssLength").value;  
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber +1);
+    }
+    document.getElementById("password").value = password;
+} */
+
+/*function generate(){
+    var lowercaseSet = document.getElementById("lowercase").checked;
+    console.log(lowercaseSet)
+}*/
+
+//var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
