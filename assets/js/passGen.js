21b8f931bfec3c26736299eaf741934a366cdca0
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-01-19 20:56:50 -0500

    Fixed all of the error handling, fixed issue where length would always be plus 1 actual amount

--- assets/js/passGen.js
+++ assets/js/passGen.js
@@ -1,12 +1,5 @@
 
-/*var password=document.getElementById("pssLength");
-
-function generate() {
-    var passwordLength = document.getElementById("pssLength").value;
-    console.log("Value is " + passwordLength);
-}*/
-
-var password=document.getElementById("password");
+var password = document.getElementById("password");
 
 function generate() {
     var password = "";
@@ -14,7 +7,7 @@ function generate() {
     var uppercaseSet = document.getElementById("uppercase").checked;
     var numericSet = document.getElementById("numeric").checked;
     var specialSet = document.getElementById("special").checked;
-    var passwordLength = document.getElementById("pssLength").value;  
+    var passwordLength = document.getElementById("pssLength").value - 1;  
     var chars = "";
     if(lowercaseSet){
         chars += "abcdefghijklmnopqrstuvwxyz";
@@ -23,38 +16,20 @@ function generate() {
         chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
     if(numericSet){
-        chars += "0123456789"
+        chars += "0123456789";
     }
     if(specialSet){
-        chars += "!@#$%^&*()"
+        chars += "!@#$%^&*()";
     }
     if(chars === ""){
-        window.alert("Must which characters to include")
+        window.alert("Must check which characters to include!");
+    } else if(passwordLength < 8 || passwordLength > 128){
+        window.alert("Password length must be 8 - 128 characters!");
     }
     for (var i = 0; i <= passwordLength; i++) {
         var randomNumber = Math.floor(Math.random() * chars.length);
         password += chars.substring(randomNumber, randomNumber +1);
     }
+    
     document.getElementById("password").value = password;
 }
-
-
-/*
-function generate() {
-    var password = "";
-    //var passwordLength = 8;
-    var passwordLength = document.getElementById("pssLength").value;  
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    for (var i = 0; i <= passwordLength; i++) {
-        var randomNumber = Math.floor(Math.random() * chars.length);
-        password += chars.substring(randomNumber, randomNumber +1);
-    }
-    document.getElementById("password").value = password;
-} */
-
-/*function generate(){
-    var lowercaseSet = document.getElementById("lowercase").checked;
-    console.log(lowercaseSet)
-}*/
-
-//var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
